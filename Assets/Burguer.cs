b6a54646b6bdeea88810c2de447d087a12426481
Enric Ayguadé <enricayguade@gmail.com>
2015-11-27 13:39:43 +0100

    burguer added

---
+++ Assets/Burguer.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Burguer : MonoBehaviour {
+
+	public float degreesY = 3.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(0,degreesY,0,Space.World);
+	}
+}
