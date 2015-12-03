37d3164ba59f57c7c5ab9bf32d122f9afb628f2b
Enric Ayguadé <enricayguade@gmail.com>
2015-12-03 14:24:15 +0100

    stamina bar

--- Assets/Scripts/GameMaster.cs
+++ Assets/Scripts/GameMaster.cs
@@ -7,14 +7,22 @@ public class GameMaster : MonoBehaviour {
 	public Text staminaText;
 	private static bool isGameOver;
 
+	public Image StaminaBar;
+	float originalStaminaWidth;
+	float originalStaminaX;
+
 	// Use this for initialization
 	void Start () {
 		isGameOver = false;
+		originalStaminaWidth = StaminaBar.rectTransform.sizeDelta.x;
+		originalStaminaX = StaminaBar.rectTransform.anchoredPosition.x;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		staminaText.text = "Stamina: " + ((int) Player.stamina).ToString ();
+		//staminaText.text = "Stamina: " + ((int) Player.stamina).ToString ();
+		StaminaBar.rectTransform.sizeDelta = new Vector2 ((Player.stamina*originalStaminaWidth)/100.0f, StaminaBar.rectTransform.sizeDelta.y);
+		StaminaBar.rectTransform.anchoredPosition = new Vector2 (originalStaminaX, StaminaBar.rectTransform.anchoredPosition.y);
 		if (isGameOver && Time.timeScale > 0) {
 			//if (Time.timeScale > 0.1) Time.timeScale -= 0.001f;
 			//else Time.timeScale = 0;
