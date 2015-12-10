0b89d5fcda07811cdbf08f9f1decc812c2769854
Enric Ayguadé <enricayguade@gmail.com>
2015-12-10 17:33:18 +0100

    menu completed moreless

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -6,11 +6,30 @@ public class Menu : MonoBehaviour {
 
 	public Button PlayB;
 
+	public Button ControlsB;
+	public Image ControlsInfoImage;
+	bool areControlsShowing;
+
+	public Button AboutB;
+	public Image AboutInfoImage;
+	bool isAboutShowing;
+
 	void Start () {
 		PlayB.onClick.AddListener (() => {
 			PlayClick ();
 		});
 
+		ControlsB.onClick.AddListener (() => {
+			ControlsClick (); 
+		});
+		areControlsShowing = false;
+		ControlsInfoImage.gameObject.SetActive (false);
+
+		AboutB.onClick.AddListener (() => {
+			AboutClick (); 
+		});
+		isAboutShowing = false;
+		AboutInfoImage.gameObject.SetActive (false);
 	}
 	
 	// Update is called once per frame
@@ -18,7 +37,19 @@ public class Menu : MonoBehaviour {
 
 	}
 
-	public static void PlayClick() {
+	void PlayClick() {
 		Application.LoadLevel ("scene1");
+	}
+
+	void ControlsClick() {
+		if (areControlsShowing) ControlsInfoImage.gameObject.SetActive (false);
+		else ControlsInfoImage.gameObject.SetActive (true);
+		areControlsShowing = !areControlsShowing;
+	}
+
+	void AboutClick() {
+		if (isAboutShowing) AboutInfoImage.gameObject.SetActive (false);
+		else AboutInfoImage.gameObject.SetActive (true);
+		isAboutShowing = !isAboutShowing;
 	}
 }
---
+++ Assets/Scripts/Trees.cs
@@ -0,0 +1,95 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Trees : MonoBehaviour {
+
+	public GameObject tree;
+	List<GameObject> trees;
+
+	float leftXTree = 8.0f;
+	float rightXTree = 14.0f;
+
+	public GameObject plane;
+	List<GameObject> planes;
+
+	// Use this for initialization
+	void Start () {
+		trees = new List<GameObject> ();
+		planes = new List<GameObject> ();
+		InvokeRepeating ("createTreeAtRandomTime", 0.0f, 1.0f);
+		createFirstPlanes ();
+		createPlane ();
+		InvokeRepeating ("createPlane", 0.0f, 5.25f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		updatePositions ();
+		deleteOldTrees ();
+		deleteOldPlanes ();
+	}
+
+	void createTreeAtRandomTime() {
+		Invoke ("createTree", Random.Range (1.2f, 2.5f));
+	}
+
+	void createTree() {
+		int side = Random.Range (1, 3);
+		float treeX;
+		if (side == 1)
+			treeX = leftXTree;
+		else
+			treeX = rightXTree;
+
+		GameObject treeClone = (GameObject) Instantiate (tree, new Vector3 (treeX, 0, 25), new Quaternion(0,0,0,0));
+		trees.Add (treeClone);
+	}
+
+	void createFirstPlanes() {
+		GameObject planeClone = (GameObject) Instantiate (plane, new Vector3 (7.5f, 0.0f, 5.0f), new Quaternion (0, 0, 0, 0));
+		GameObject planeClone2 = (GameObject) Instantiate (plane, new Vector3 (7.5f, 0.0f, 35.0f), new Quaternion (0, 0, 0, 0));
+		planes.Add (planeClone);
+		planes.Add (planeClone2);
+	}
+
+	void createPlane() {
+		GameObject planeClone = (GameObject) Instantiate (plane, new Vector3 (7.5f, 0.0f, 65.0f), new Quaternion (0, 0, 0, 0));
+		planes.Add (planeClone);
+	}
+
+	void updatePositions() {
+		foreach (GameObject aTree in trees) {
+			aTree.transform.position -= new Vector3(0.0f, 0.0f, 0.1f);
+		}
+		foreach (GameObject aPlane in planes) {
+			aPlane.transform.position -= new Vector3(0.0f, 0.0f, 0.1f);
+		}
+	}
+
+	void deleteOldTrees() {
+		List<GameObject> treesToDelete = new List<GameObject> ();
+		foreach (GameObject aTree in trees) {
+			if (aTree.transform.position.z < 0) {
+				treesToDelete.Add(aTree);
+			}
+		} 
+		foreach (GameObject treeToDelete in treesToDelete) {
+			Destroy(treeToDelete);
+			trees.Remove(treeToDelete);
+		}
+	}
+
+	void deleteOldPlanes() {
+		List<GameObject> planesToDelete = new List<GameObject> ();
+		foreach (GameObject aPlane in planes) {
+			if (aPlane.transform.position.z < -15) {
+				planesToDelete.Add(aPlane);
+			}
+		} 
+		foreach (GameObject planeToDelete in planesToDelete) {
+			Destroy(planeToDelete);
+			planes.Remove(planeToDelete);
+		}
+	}
+}
